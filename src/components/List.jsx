9da37192984eadd5ea379c13f5ca9c7dd726f367
Syed Raza <cute.lorian17@gmail.com>
2019-10-16 15:48:35 +0500

    react first app of adding ingredients list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,21 +1,17 @@
-var React = require("react");
-var ListItem = require("./ListItem.jsx");
-
-var ingredient = [
-  { id: 1, text: "raza" },
-  { id: 2, text: "zain" },
-  { id: 3, text: "ammar" },
-  { id: 4, text: "mahnoor" }
-];
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
 
 var List = React.createClass({
-  render: function() {
-    var listItems = ingredient.map(function(item) {
-      return <ListItem key={item.id} ingredient={item.text} />;
-    });
-
-    return <ul>{listItems}</ul>;
-  }
+    render: function () {
+        var createItem = function (text, index) {
+            return (
+                <ListItem key={index + text} text={text} />
+            );
+        };
+        return (
+            <ul>{this.props.items.map(createItem)}</ul>
+        );
+    }
 });
 
 module.exports = List;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var List = require("./components/List.jsx");
+var ListManager = require("./components/ListManager.jsx");
 
-ReactDOM.render(<List />, document.getElementById("ingredient"));
+ReactDOM.render(<ListManagery title="Ingredients Initial" />, document.getElementById("ingredient"));
