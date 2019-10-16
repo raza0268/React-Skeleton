7b5bc4fdb6f39936559ba3dcf09afa47dc593228
Syed Raza <cute.lorian17@gmail.com>
2019-10-16 16:02:53 +0500

    file changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 var ListManager = require("./components/ListManager.jsx");
 
-ReactDOM.render(<ListManagery title="Ingredients Initial" />, document.getElementById("ingredient"));
+ReactDOM.render(<ListManager title="Ingredients Initial" />, document.getElementById("ingredient"));
